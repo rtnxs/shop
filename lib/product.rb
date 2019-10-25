48513b95d766954d334e97712033c977b9bc70a0
rtnxs <riotnexus@mail.ru>
2019-10-25 12:17:53 +0300

    refactored according to code style

--- lib/product.rb
+++ lib/product.rb
@@ -1,13 +1,13 @@
 class Product
   attr_accessor :price, :rest
 
+  def self.from_file(file_path)
+    raise NotImplementedError
+  end
+
   def initialize(params)
     @rest  = params[:rest].to_i
     @price = params[:price].to_f
-  end
-
-  def self.from_file(file_path)
-    raise NotImplementedError
   end
 
   def to_s
